e5820abc0660d0430d5b7d3d4e41e88e114caa73
Jamezfn <jamesmakachia19@gmail.com>
2025-07-05 13:34:09 +0300

    14. Longest Common Prefix

--- 14. Longest Common Prefix/optimized.py
+++ 14. Longest Common Prefix/optimized.py
@@ -3,16 +3,14 @@ from typing import List
 def longestCommonPrefix(strs: List[str]) -> str:
     max_len = min(len(s) for s in strs)
     low, high = 0, max_len
-    lcp_len = 0
     while low < high:
-        mid = (low + high) // 2
+        mid = (low + high + 1) // 2
         prefix = strs[0][:mid]
         if all(s.startswith(prefix) for s in strs):
-            lcp_len = mid
-            low = mid + 1
+            low = mid
         else:
             high = mid - 1
-    return strs[0][:lcp_len]
+    return strs[0][:high]
 
 print(longestCommonPrefix(["flower","flow","flight"]))
 print(longestCommonPrefix(["dog","racecar","car"]))
